844ad0ca327a5403d83a289bf3f8081d0712310c
Nihansadamdini <dilninihansa@gmail.com>
2026-01-12 09:06:16 +0530

    Add files via upload

---
+++ InsertionSort.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author Nihansa
+ */
+public class InsertionSort {
+
+    public static void main(String[] args) {
+        int[] arr = {9, 5, 1, 4, 3};
+
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+
+            arr[j + 1] = key;
+        }
+
+        System.out.println("Sorted Array:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
+
