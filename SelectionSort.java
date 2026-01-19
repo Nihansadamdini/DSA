c633d7b9ac880cfb4342c19da29cbd8973b142d3
Nihansadamdini <dilninihansa@gmail.com>
2026-01-20 04:47:38 +0530

    Add files via upload

---
+++ SelectionSort.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author Nihansa
+ */
+public class SelectionSort {
+
+    public static void main(String[] args) {
+        int[] arr = {64, 25, 12, 22, 11};
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIndex = i;
+
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            // swap
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+
+        System.out.println("Sorted Array:");
+        for (int n : arr) {
+            System.out.print(n + " ");
+        }
+    }
+}
+
+
+    
